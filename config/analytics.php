fbbbea93e143553049a15e67919b2c9a10cc7b96
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-24 21:05:40 +0430

    feat: add analytics.php

---
+++ config/analytics.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'enabled' => env('ANALYTICS_ENABLED', true),
+    'visitor_cookie' => 'bos_vid',
+    'cookie_days' => 400,
+    'country_headers' => array_values(array_filter([
+        env('ANALYTICS_COUNTRY_HEADER'),
+        'CF-IPCountry',
+        'CloudFront-Viewer-Country',
+        'X-Vercel-IP-Country',
+        'X-Country-Code',
+    ])),
+];
