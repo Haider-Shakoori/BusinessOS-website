d9a5f88b8095986fc971c0663ce8d8e96ba7266f
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 02:38:51 +0430

    feat: complete resources/views/seo/sitemap.blade.php

--- resources/views/seo/sitemap.blade.php
+++ resources/views/seo/sitemap.blade.php
@@ -1,11 +1,17 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
 @foreach ($urls as $url)
     <url>
         <loc>{{ $url['loc'] }}</loc>
         <lastmod>{{ $url['lastmod'] }}</lastmod>
         <priority>{{ $url['priority'] }}</priority>
+        <xhtml:link rel="alternate" hreflang="en" href="{{ $url['loc'] }}"/>
+        <xhtml:link rel="alternate" hreflang="fa-AF" href="{{ $url['loc'] }}?lang=fa"/>
+        <xhtml:link rel="alternate" hreflang="ps-AF" href="{{ $url['loc'] }}?lang=ps"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="{{ $url['loc'] }}"/>
     </url>
 @endforeach
 </urlset>
+
+[executed on device: ubuntu-6gb-dal-x8mx (c447f909-fdcc-4121-9924-27a69d35e9b2)]
