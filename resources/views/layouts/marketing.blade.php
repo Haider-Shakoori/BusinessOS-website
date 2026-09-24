5d94b16e6905b21d44046210860a8bb3936ed39a
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-24 23:38:25 +0430

    feat: modernize BusinessOS responsive marketing UI
    Refine the flat BusinessOS marketing experience with standard SaaS spacing, typography, cards and native responsive reflow across desktop, tablet and mobile while preserving CMS, analytics, SEO and performance.

--- resources/views/layouts/marketing.blade.php
+++ resources/views/layouts/marketing.blade.php
@@ -20,13 +20,13 @@
     <meta name="twitter:description" content="{{ $meta['description'] }}">
 
     <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
-    <link rel="stylesheet" href="{{ asset('assets/css/businessos.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/businessos.css') }}?v={{ filemtime(public_path('assets/css/businessos.css')) }}">
 
     @foreach ($schema ?? [] as $entity)
         <script type="application/ld+json">{!! json_encode($entity, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) !!}</script>
     @endforeach
 </head>
-<body>
+<body class="modern-site">
     <a class="skip-link" href="#main">Skip to content</a>
 
     <header class="site-header">
