ade436e80bc615a8cbe438484386c37b9c1b50aa
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-24 21:08:17 +0430

    feat: add resources and CMS routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\AnalyticsController as AdminAnalyticsController;
+use App\Http\Controllers\Admin\AuthController as AdminAuthController;
+use App\Http\Controllers\Admin\GuideController as AdminGuideController;
+use App\Http\Controllers\GuideController;
 use App\Http\Controllers\InquiryController;
 use App\Http\Controllers\MarketingController;
 use App\Http\Controllers\SeoController;
@@ -12,6 +16,9 @@ Route::get('/apps/{slug}', [MarketingController::class, 'show'])
     ->where('slug', '[a-z0-9-]+')
     ->name('apps.show');
 
+Route::get('/resources', [GuideController::class, 'index'])->name('resources.index');
+Route::get('/guides/{guide:slug}', [GuideController::class, 'show'])->name('resources.show');
+
 Route::get('/pricing', [MarketingController::class, 'pricing'])->name('pricing');
 Route::get('/about', [MarketingController::class, 'about'])->name('about');
 Route::get('/security', [MarketingController::class, 'security'])->name('security');
@@ -22,6 +29,20 @@ Route::get('/request-demo', [MarketingController::class, 'demo'])->name('demo');
 Route::post('/contact', [InquiryController::class, 'store'])
     ->middleware('throttle:5,1')
     ->name('inquiries.store');
+
+Route::middleware('guest')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/login', [AdminAuthController::class, 'create'])->name('login');
+    Route::post('/login', [AdminAuthController::class, 'store'])
+        ->middleware('throttle:5,1')
+        ->name('login.store');
+});
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminAnalyticsController::class, 'dashboard'])->name('dashboard');
+    Route::get('/analytics', [AdminAnalyticsController::class, 'index'])->name('analytics');
+    Route::resource('guides', AdminGuideController::class)->except(['show']);
+    Route::post('/logout', [AdminAuthController::class, 'destroy'])->name('logout');
+});
 
 Route::get('/sitemap.xml', [SeoController::class, 'sitemap'])->name('sitemap');
 Route::get('/robots.txt', [SeoController::class, 'robots'])->name('robots');
