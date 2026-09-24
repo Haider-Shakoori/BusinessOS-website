f3ae8b319d4309c1ebb51a48400eb9dc184a0de1
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 02:36:07 +0430

    feat: complete platform work in database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,8 +9,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SiteSettingSeeder::class,
             ProductSeeder::class,
             GuideSeeder::class,
         ]);
     }
 }
+
+[executed on device: ubuntu-6gb-dal-x8mx (c447f909-fdcc-4121-9924-27a69d35e9b2)]
