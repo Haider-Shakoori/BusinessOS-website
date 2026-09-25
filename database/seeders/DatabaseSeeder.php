ef58792d0e06f1a53becde38100ac94ba87939ef
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 21:50:20 +0430

    Seed search landing pages and expanded guide library

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
             SiteSettingSeeder::class,
             ProductSeeder::class,
             GuideSeeder::class,
+            SearchGuideSeeder::class,
+            SeoPageSeeder::class,
         ]);
     }
 }
