aa39aa7cfbf2ce924d938f50c5de501b9b9721d8
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-25 22:21:13 +0430

    Emit image metadata in product sitemap entries

--- resources/views/seo/sitemap.blade.php
+++ resources/views/seo/sitemap.blade.php
@@ -1,6 +1,6 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 @foreach ($urls as $url)
     <url>
         <loc>{{ $url['loc'] }}</loc>
@@ -10,6 +10,13 @@
         <xhtml:link rel="alternate" hreflang="fa-AF" href="{{ $url['loc'] }}?lang=fa"/>
         <xhtml:link rel="alternate" hreflang="ps-AF" href="{{ $url['loc'] }}?lang=ps"/>
         <xhtml:link rel="alternate" hreflang="x-default" href="{{ $url['loc'] }}"/>
+        @foreach($url['images'] ?? [] as $image)
+            <image:image>
+                <image:loc>{{ $image['loc'] }}</image:loc>
+                @if($image['title'])<image:title>{{ $image['title'] }}</image:title>@endif
+                @if($image['caption'])<image:caption>{{ $image['caption'] }}</image:caption>@endif
+            </image:image>
+        @endforeach
     </url>
 @endforeach
 </urlset>
