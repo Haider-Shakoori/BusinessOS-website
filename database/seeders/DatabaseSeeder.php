10f858068aac90f890ed02e1a46eb0b06a36bf9a
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-26 05:41:24 +0430

    Seed verified case studies by default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             GuideSeeder::class,
             SearchGuideSeeder::class,
             SeoPageSeeder::class,
+            CaseStudySeeder::class,
         ]);
     }
 }
